f88b70beda6d5bb2da13d2d71d0938ddad2314f2
username <ganieviko@gmail.com>
2022-10-01 21:24:09 -0400

    second commit another

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,35 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class SimpleTest {
+    @Test
+    public void test1() {
+        System.out.println("Success");
+    }
+    @Test
+    public void test2() {
+        System.out.println("Success");
+    }
+    @Test
+    public void test3() {
+        boolean failOrNotFail = new Random().nextBoolean();
+        if(failOrNotFail) {
+            Assert.fail("Fail 3");
+        } else {
+            System.out.println("Success 3");
+        }
+    }
+
+    @Test(enabled = false)
+    public void test4() {
+        Assert.fail("Fail 4");
+    }
+
+    @Test
+    public void test5() {
+        System.out.println("Success5");
+    }
+
+}
